22368f4bcd8397dd1e1739f9d1147c1eb547edb3
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-08 15:26:46 +0400

    bug

--- crontab/deleteProduct.php
+++ crontab/deleteProduct.php
@@ -1,9 +1,2 @@
 <?php
 
-// require database
-require_once "admin/app/classes/database.php";
-// require databasefunction
-require_once "admin/app/classes/productFunction.php";
-
-
-deleteOverdueProduct();
