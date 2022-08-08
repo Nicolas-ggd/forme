afda80ebafcfa2cd717ee31b8ec0368392a45273
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-08 15:27:22 +0400

    add 404page

---
+++ admin/view/pages/404Page.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Products</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/public/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/dist/css/adminlte.min.css?v=3.2.0">
+    <link rel="stylesheet" href="/public/lib/suite_gpl/codebase/suite.css">
+
+    <script src="/public/plugins/jquery/jquery.min.js"></script>
+    <script src="/public/lib/CodeSeven-toastr-2.1.4-7-g50092cc/CodeSeven-toastr-50092cc/toastr.js"></script>
+
+    <script src="/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="/public/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="/public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/public/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="/public/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="/public/plugins/jszip/jszip.min.js"></script>
+    <script src="/public/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="/public/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+    <script src="/public/dist/js/adminlte.min.js?v=3.2.0"></script>
+
+    <script src="/public/dist/js/demo.js"></script>
+    <!-- checkbox select js -->
+    <script src="/public/js/checkboxSelect.js"></script>
+    <!-- Datapicker calendar js -->
+    <script src="/public/js/datepicker.js"></script>
+    <!-- Request js -->
+    <script src="/public/js/request.js"></script>
+    <!-- suite gpl calendar -->
+    <script src="/public/lib/suite_gpl/codebase/suite.js"></script>
+</head>
+<body>
+        <!-- Main content -->
+        <section class="content">
+            <div class="error-page">
+                <h2 class="headline text-warning"> 404</h2>
+                <div class="error-content">
+                    <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+                    <p>
+                        We could not find the page you were looking for.
+                        Meanwhile, you may <a href="../../index.html">return to dashboard</a> or try using the search form.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </section>
+
+
+</body>
+</html>
+
+
+<!-- Site wrapper -->
+
