5d06aabe771d89e9acdbdab7c2362065f3eb1265
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-08 15:24:51 +0400

    save product table sql

---
+++ admin/App/SQL/createProductTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE product(
+                        id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+                        product_name VARCHAR(90) NOT NULL,
+                        product_desc VARCHAR(120) NOT NULL,
+                        product_price DECIMAL (10,2) NOT NULL,
+                        product_status VARCHAR(50) NOT NULL,
+                        product_deadline DATE NOT NULL
+
+);
