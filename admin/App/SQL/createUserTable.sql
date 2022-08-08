3d0f9d248633f657e4f0ab5de0035ca296a3f122
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-08 15:23:23 +0400

    add user table sql

---
+++ admin/App/SQL/createUserTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+                       user_name VARCHAR(50) NOT NULL,
+                       user_lastname VARCHAR(80) NOT NULL,
+                       user_email VARCHAR(80) UNIQUE NOT NULL,
+                       user_password VARCHAR(80) NOT NULL
+)
