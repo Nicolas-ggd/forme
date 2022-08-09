79a9acd251f21712508ac0e1288a6d7470446e56
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-09 11:34:47 +0400

    remove filter function
    adminlte3 already have filter function

--- public/js/filter.js
+++
@@ -1,7 +0,0 @@
-// search box filter
-$("#search_input").on("keyup", function () {
-    let value = $(this).val().toUpperCase();
-    $("#t_body tr").filter(function () {
-        $(this).toggle($(this).text().toUpperCase().indexOf(value) > -1)
-    });
-});
