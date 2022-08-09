f81ca5e187ffb7e40c97eae0976bc946ae8245e1
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-09 16:41:07 +0400

    rechange name
    this is old registrationController page

---
+++ admin/App/Controller/RegistrationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Database\Product\UserRepository;
+use App\Request\RegistrationRequest;
+
+class RegistrationController
+{
+    protected $request;
+    protected $repository;
+
+    public function __construct(){
+
+        $this->request = new RegistrationRequest();
+        $this->repository = new UserRepository();
+
+    }
+
+    public function createView(){
+        require 'admin/view/pages/CreateAccount.php';
+    }
+
+    public function loginView(){
+        require 'admin/view/pages/Login.php';
+    }
+
+    public function registration(){
+        $data = $this->request->createRequest();
+
+        if(!isset($data['error'])){
+            $this->repository->insert($data['name'], $data['lastname'], $data['email'], $data['password']);
+            $data = "Success";
+        }
+
+        echo json_encode($data);
+
+    }
+
+}
