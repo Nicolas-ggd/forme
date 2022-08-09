8f96ea1df2104fa38d7350f0e4beee9e2ac480fc
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-09 16:40:28 +0400

    add login page

---
+++ admin/view/pages/Login.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/public/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="/public/dist/css/adminlte.min.css?v=3.2.0">
+    <link rel="stylesheet" href="/public/lib/suite_gpl/codebase/suite.css">
+    <link rel="stylesheet" href="/public/plugins/fontawesome-free/css/all.min.css">
+
+
+    <script src="/public/plugins/jquery/jquery.min.js"></script>
+    <script src="/public/lib/CodeSeven-toastr-2.1.4-7-g50092cc/CodeSeven-toastr-50092cc/toastr.js"></script>
+    <script src="/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/public/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="/public/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/public/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="/public/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="/public/plugins/jszip/jszip.min.js"></script>
+    <script src="/public/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="/public/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="/public/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <script src="/public/dist/js/adminlte.min.js?v=3.2.0"></script>
+    <script src="/public/dist/js/demo.js"></script>
+    <script src="/public/plugins/jquery-validation/jquery.validate.min.js"></script>
+    <script src="/public/plugins/jquery-validation/additional-methods.min.js"></script>
+
+
+</head>
+<body class="register-page iframe-mode" style="height: 100%;">
+<section class="content">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary">
+
+                    <div class="card-header">
+                        <h3 class="card-title">Quick Example <small>jQuery Validation</small></h3>
+                    </div>
+
+                    <form id="login_form" method="post">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" name="user_email" class="form-control" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" name="user_password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="form-group mb-0">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
+                                    <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms
+                                            of service</a>.</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" form="login_form" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<script>
+    $(function () {
+        $.validator.setDefaults({
+            submitHandler: function () {
+                alert("Form successful submitted!");
+            }
+        });
+        $('#quickForm').validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true,
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                terms: {
+                    required: true
+                },
+            },
+            messages: {
+                email: {
+                    required: "Please enter a email address",
+                    email: "Please enter a valid email address"
+                },
+                password: {
+                    required: "Please provide a password",
+                    minlength: "Your password must be at least 5 characters long"
+                },
+                terms: "Please accept our terms"
+            },
+            errorElement: 'span',
+            errorPlacement: function (error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+    });
+</script>
+</body>
+</html>
