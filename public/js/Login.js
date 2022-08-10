41b871e1f812de955fc4c722b3a7811e8ee850fb
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-10 14:07:56 +0400

    create login request js

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
 
-    $("#user_form").submit(function (e) {
+    $("#login_form").submit(function (e) {
 
         let params = $(this).serialize();
         e.preventDefault();
 
-        $.post("?action=userLogin", params)
+        $.post("?ajax=userLogin", params)
             .done(function (response) {
                 if (response) {
                     $(document).Toasts('create', {
                         class: 'bg-success',
                         title: 'Success',
                         subtitle: 'Subtitle',
-                        body: 'Account successful created'
+                        body: 'Account successful Verified'
                     })
 
                     setTimeout(function () {
-                        window.location.href = '?action=back';
+                        window.location.href = '/';
                     }, 1000)
                 }
             })
 
             .fail(function (response) {
-                // let data = JSON.parse(response.responseText);
-                // data.error.forEach(error => {
-                //
-                //     $(document).Toasts('create', {
-                //         class: 'bg-danger',
-                //         title: 'Error',
-                //         subtitle: 'Subtitle',
-                //         body: error
-                //     })
-                // })
+                let data = JSON.parse(response.responseText);
+                data.error.forEach(error => {
+
+                    $(document).Toasts('create', {
+                        class: 'bg-danger',
+                        title: 'Error',
+                        subtitle: 'Subtitle',
+                        body: error
+                    })
+                })
             })
 
     });
