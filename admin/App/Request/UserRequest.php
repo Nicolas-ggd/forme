e9a8a230702cd4d7b75381e256ef2d5116ec557b
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-12 09:50:15 +0400

    create users profile image uploader function
    this function is invalid now

--- admin/App/Request/UserRequest.php
+++ admin/App/Request/UserRequest.php
@@ -5,4 +5,59 @@ namespace App\Request;
 class UserRequest
 {
 
+    public function aboutUser()
+    {
+
+        $error = [];
+
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["fileToUpload"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                $error[] = 'File is an image - " . $check["mime"] . "."';
+                $uploadOk = 1;
+            } else {
+                $error[] = "Missed File";
+                $uploadOk = 0;
+            }
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            $error[] = "Sorry, file already exists";
+            $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            $error[] = "Sorry, your file is too large";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            $error[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            $error[] = "Sorry, your file was not uploaded";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+            } else {
+                $error[] = "Sorry, there was an error uploading your file";
+            }
+        }
+
+    }
+
 }
