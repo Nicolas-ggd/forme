e2117ca887c463c08dd39df8337675da281f0522
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-12 09:51:48 +0400

    remove header and side navbar

--- admin/view/homepage.php
+++ admin/view/homepage.php
@@ -18,251 +18,14 @@
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
+
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="../../index3.html" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Contact</a>
-            </li>
-        </ul>
+    <?php require 'components/Header.php'; ?>
+    <!-- Navbar -->
 
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Navbar Search -->
-            <li class="nav-item">
-                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i class="fas fa-search"></i>
-                </a>
-                <div class="navbar-search-block">
-                    <form class="form-inline">
-                        <div class="input-group input-group-sm">
-                            <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                                   aria-label="Search">
-                            <div class="input-group-append">
-                                <button class="btn btn-navbar" type="submit">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </li>
-
-            <!-- Messages Dropdown Menu -->
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="far fa-comments"></i>
-                    <span class="badge badge-danger navbar-badge">3</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="../../dist/img/user1-128x128.jpg" alt="User Avatar"
-                                 class="img-size-50 mr-3 img-circle">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    Brad Diesel
-                                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">Call me whenever you can...</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="../../dist/img/user8-128x128.jpg" alt="User Avatar"
-                                 class="img-size-50 img-circle mr-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    John Pierce
-                                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">I got your message bro</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                            <img src="../../dist/img/user3-128x128.jpg" alt="User Avatar"
-                                 class="img-size-50 img-circle mr-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    Nora Silvester
-                                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">The subject goes here</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                        <!-- Message End -->
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                </div>
-            </li>
-
-            <!-- User -->
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="fas fa-user"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-
-                    <span class="dropdown-item dropdown-header">
-                        <?php echo isset($_SESSION['user_name']) ? $_SESSION['user_name'] .' '. $_SESSION['user_lastname'] : 'user'; ?></span>
-                    <div class="dropdown-divider"></div>
-
-                    <a href="#" class="dropdown-item">
-                        <i class="mx-1 bi bi-envelope"></i><?php echo isset($_SESSION['user_email']) ? $_SESSION['user_email'] : 'user'; ?>
-                    </a>
-                    <a href="?action=profile" class="dropdown-item">
-                        <i class="bi bi-person mx-1"></i> Profile
-                    </a>
-                    <a href="#" class="dropdown-item">
-                        <i class="bi bi-gear mx-1"></i> Settings
-                    </a>
-                    <a href="?action=create" class="dropdown-item">
-                        <i class="bi bi-box-arrow-right mx-1"></i> Log Out
-                        <span class="float-right text-muted text-sm"></span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                </div>
-            </li>
-            <!-- User -->
-
-            <!-- Notifications Dropdown Menu -->
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="far fa-bell"></i>
-                    <span class="badge badge-warning navbar-badge">15</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <span class="dropdown-item dropdown-header">15 Notifications</span>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fas fa-envelope mr-2"></i> 4 new messages
-                        <span class="float-right text-muted text-sm">3 mins</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fas fa-users mr-2"></i> 8 friend requests
-                        <span class="float-right text-muted text-sm">12 hours</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <i class="fas fa-file mr-2"></i> 3 new reports
-                        <span class="float-right text-muted text-sm">2 days</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                    <i class="fas fa-expand-arrows-alt"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                    <i class="fas fa-th-large"></i>
-                </a>
-            </li>
-        </ul>
-    </nav>
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="../../index3.html" class="brand-link">
-
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
-        </a>
-
-        <!-- Sidebar -->
-        <div class="sidebar">
-            <!-- Sidebar user (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="/public/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#"
-                       class="d-block"><?php echo isset($_SESSION['user_name']) ? $_SESSION['user_name'] .' '. $_SESSION['user_lastname'] : 'user'; ?></a>
-                </div>
-            </div>
-
-            <!-- SidebarSearch Form -->
-            <div class="form-inline">
-                <div class="input-group" data-widget="sidebar-search">
-                    <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                           aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-sidebar">
-                            <i class="fas fa-search fa-fw"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                    data-accordion="false">
-                    <!-- Add icons to the links using the .nav-icon class
-                         with font-awesome or any other icon font library -->
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>
-                                Dashboard
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="../../index.html" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v1</p>
-                                </a>
-                            </li>
-
-                        </ul>
-                    </li>
-
-
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon far fa-circle text-info"></i>
-                            <p>Informational</p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-    </aside>
+    <!-- Side Navbar -->
+    <?php require 'components/SideNavbar.php'; ?>
+    <!-- Side Navbar -->
 
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
@@ -288,7 +51,6 @@
             <!-- Default box -->
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Title</h3>
 
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
@@ -306,8 +68,7 @@
                                 <div class="col-12">
                                     <div class="card">
                                         <div class="card-header">
-                                            <h3 class="card-title">DataTable with minimal features &amp; hover
-                                                style</h3>
+                                            <h3 class="card-title">პროდუქტის თეიბლი</h3>
                                         </div>
 
 
@@ -315,17 +76,17 @@
                                             <div class="d-flex align-items-center justify-content-between">
                                                 <div class="">
                                                     <a href="?action=addProduct"
-                                                       class="btn btn-outline-info my-2 btn-rounded">Add Product</a>
+                                                       class="btn btn-outline-info my-2 btn-rounded">პროდუქტის დამატება</a>
 
                                                     <button type="button"
                                                             class="h-20 btn btn-outline-warning btn-rounded mass">
-                                                        Mass Delete
+                                                        მასიური წაშლა
                                                     </button>
 
                                                     <button type="button"
                                                             class="d-none mass_delete btn btn-outline-success btn-rounded">
                                                         <i
-                                                                class="mx-1 ties bi bi-check-lg"></i>Confirm
+                                                                class="mx-1 ties bi bi-check-lg"></i>თანხმობა
                                                     </button>
                                                 </div>
                                             </div>
@@ -353,27 +114,27 @@
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
                                                                         aria-label="Browser: activate to sort column ascending">
-                                                                        Product Name
+                                                                        პროდუქტის დასახელება
                                                                     </th>
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
                                                                         aria-label="Platform(s): activate to sort column ascending">
-                                                                        Product Description
+                                                                        პროდუქტის აღწერა
                                                                     </th>
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
                                                                         aria-label="Platform(s): activate to sort column ascending">
-                                                                        Product Price
+                                                                        პროდუქტის ფასი
                                                                     </th>
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
                                                                         aria-label="Engine version: activate to sort column ascending">
-                                                                        Product Status
+                                                                        სტატუსი
                                                                     </th>
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
                                                                         aria-label="CSS grade: activate to sort column ascending">
-                                                                        Duration
+                                                                        ხანგრძლივობა
                                                                     </th>
                                                                     <th class="sorting" tabindex="0"
                                                                         aria-controls="example2" rowspan="1" colspan="1"
@@ -446,7 +207,7 @@
                                                                                         value="<?php echo $product['id']; ?>"
                                                                                         class="d-none btn btn-success confirm btn-rounded">
                                                                                     <i class="ties bi bi-check-lg"></i>
-                                                                                    
+
                                                                                 </button>
                                                                             </td>
                                                                         </tr>
