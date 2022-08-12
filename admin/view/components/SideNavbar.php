560eb584a3fe3e81eb1cf7f2657ff4e07b8309bd
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-12 18:22:57 +0400

    added permision href

--- admin/view/components/SideNavbar.php
+++ admin/view/components/SideNavbar.php
@@ -99,7 +99,7 @@
 
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="?action=permision" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>უფლებები</p>
                             </a>
