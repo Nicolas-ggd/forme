56e1a942640bc8dfa8592a87ed46f0dafb1a6b74
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-12 18:22:04 +0400

    create footer page

---
+++ admin/view/components/Footer.php
@@ -0,0 +1,11 @@
+<footer class="top"
+        style="background-color: rgb(0 102 204); height: 50px; z-index: 9999999; color: rgb(255 255 255); position: fixed; display: flex; align-items: center; width: 100vw; justify-content: center; bottom: 0;">
+    <div class=" d-flex align-items-center">
+
+                <span style="font-size: 1.2rem;" class="mb-3 mb-md-0 text-warning">🇬🇪 Georgian people stand with 🇺🇦Ukraine.
+                    <span class="mb-3 mb-md-0 text-light">Petition your leads. Show your support.</span>
+                </span>
+
+    </div>
+
+</footer>
