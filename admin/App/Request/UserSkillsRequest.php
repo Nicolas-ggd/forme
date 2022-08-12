5226423eb67f31f0b862f571fd669efd408f690d
Nicolas-ggd <98224439+Nicolas-ggd@users.noreply.github.com>
2022-08-12 11:26:58 +0400

    correct skills as user_skills

--- admin/App/Request/UserSkillsRequest.php
+++ admin/App/Request/UserSkillsRequest.php
@@ -10,11 +10,11 @@ class UserSkillsRequest
 
         $data = [];
 
-        if (isset($_POST['skills'])) {
+        if (isset($_POST['user_skills'])) {
             $data['id'] = $_POST['skills_id'];
-            $data['skills'] = $_POST['skills'];
+            $data['skills'] = $_POST['user_skills'];
 
-            if (empty($_POST['skills'])) {
+            if (empty($_POST['user_skills'])) {
                 $data['error'] = "Error, Please choose skill(s)";
                 http_response_code(400);
             }
